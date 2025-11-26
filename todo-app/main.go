9dd9d66ad4405662b83100c85b91037782871a44
narayana-109 <narayanapv777@gmail.com>
2025-11-27 00:50:52 +0530

    Added updateTodo and home.tpl for frontend

--- todo-app/main.go
+++ todo-app/main.go
@@ -132,6 +132,41 @@ func deleteTodo(w http.ResponseWriter, r *http.Request){
 	})
 }
 
+func updateTodo(w http.ResponseWriter, r *http.Request){
+	id := strings.TrimSpace(chi.URLParam(r, "id"))
+
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message":"The id is invalid",
+		})
+		return
+	}
+	var t todo
+	if err := json.NewDecoder(r.Body).Decode(&t); err!= nil{
+		rnd.JSON(w, http.StatusProcessing, err)
+		return
+	}
+
+	if t.Title ==""{
+		rnd.JSON(w, http.StatusBadRequest, renderer.M{
+			"message":"the title field is required",
+		})
+		return
+	}
+
+	if err:=db.C(collectionName).
+	Update(
+		bson.M{"_id":bson.ObjectIdHex(id)},
+		bson.M{"title":t.Title, "completed": t.Completed},
+	); err!= nil{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"message": "Failed to update todo",
+			"error": err,
+		})
+		return
+	}
+}
+
 func main(){
 	stopChan := make(chan os.signal)
 	signal.Notify(stopChan)

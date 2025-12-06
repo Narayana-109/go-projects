56b52895c30bc652ba95946922fb051f588b17a8
narayana-109 <narayanapv777@gmail.com>
2025-12-06 07:27:43 +0530

    Add channels

---
+++ scrape-without-library/go.mod
@@ -0,0 +1,3 @@
+module github.com/Narayana-109/go-projects/tree/main/scrape-without-library
+
+go 1.24.2
--- scrape-without-library/main.go
+++ scrape-without-library/main.go
@@ -1,10 +1,36 @@
 package ma
 
 import (
-
 	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"golang.org/x/net/html"
 )
 
 func main() {
-	fmt.Println("1st commit")
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	for _, url := range seedUrls{
+		go crawl(url, chUrls, chFinished)
+	}
+
+	for c := 0; c<len(seedUrls);{
+		select{
+		case url := <-chUrls:
+			foundUrls[url] = true
+		case <-chFinished:
+			c++
+		}
+	}
+
+	fmt.Println("\nFound", len(foundUrls), "unique urls:\n")
+	for url, _ := range foundUrls{
+		fmt.Println("-" + url)
+	}
+	close(chUrls)
 }
